f422782f58cc01f6751c144c82ba16a0ca958e55
Soumya Sinha <soumyasinha161093@gmail.com>
2026-03-27 10:15:36 +0530

    chatbot script with Langchain for streamlit

---
+++ chatbot.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+
+#load env variables
+load_dotenv()
+
+#streamlit page setup
+st.set_page_config(
+    page_title="Chatbot",
+    page_icon="🤖",
+    layout="centered"
+)
+
+st.title("💬 Generative AI Chatbot")
+
+#initiate chat history
+if "chat_history" not in st.session_state: # so that its not initiated with every interaction, still happens with reloading page
+    st.session_state.chat_history = []
+
+#show chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+#llm initiate
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0.0,
+)
+
+#input box
+user_prompt = st.chat_input("Ask Chatbot...")
+
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role":"user", "content":user_prompt})
+
+    response = llm.invoke(
+        input = [{"role":"system", "content": "You are a helpful assistant"}, *st.session_state.chat_history]
+    )
+
+    assistant_response =response.content
+    st.session_state.chat_history.append({"role":"assistant", "content": assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
+
+
+
